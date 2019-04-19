0fc87815277485b19934e28f54b1915aa4f8a212
Bartosz Sochaj <bsochaj@sigma.ug.edu.pl>
2019-04-19 20:58:33 +0200

    word2vec training with callbacks

--- scripts/cnn_w2v.py
+++ scripts/cnn_w2v.py
@@ -1,9 +1,26 @@
 from gensim.models import Word2Vec, KeyedVectors
+from gensim.models.callbacks import CallbackAny2Vec
 from glob import glob
 import os
 from keras.preprocessing import text, sequence
 
-CNN_SOURCE_FILES_PATH = "../data/cnn/cos"
+CNN_SOURCE_FILES_PATH = "../data/cnn/train"
+
+
+class Callback(CallbackAny2Vec):
+    def __init__(self):
+        self.epoch = 0
+
+    def on_epoch_end(self, model):
+        loss = model.get_latest_training_loss()
+        print('Loss after epoch {}: {}'.format(self.epoch, loss))
+        self.epoch += 1
+
+    def on_epoch_start(self, model):
+        print(f"{self.epoch} started.")
+
+    def on_train_begin(self, model):
+        print("Training started.")
 
 
 def text_generator():
@@ -14,5 +31,5 @@ def text_generator():
 
 
 iterable = text_generator()
-model = Word2Vec([item for item in iterable], size=100, window=5, min_count=1, workers=4)
+model = Word2Vec([item for item in iterable], size=100, window=5, min_count=1, workers=4, compute_loss=True, callbacks=[Callback()])
 model.wv.save_word2vec_format("cnn_w2v.model", binary=False)
