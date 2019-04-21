8df8e7ca88bf4c2e2eab96b3ad49fbfd5afdb600
Bartosz Sochaj <bsochaj@sigma.ug.edu.pl>
2019-04-21 21:45:40 +0200

    networks the first attempt

--- scripts/cnn_train_rnn.py
+++ scripts/cnn_train_rnn.py
@@ -3,6 +3,8 @@ import os
 from scripts import settings
 from scripts.cnn_datasets import prepare_dataset
 from scripts.cnn_embedings import get_word_embeddings
+from scripts.networks import cnn
+from scripts.utils import train_model
 
 train_x, train_y = prepare_dataset(
     os.path.join(settings.CNN_DATA_DIR, settings.CNN_TRAINING_LABELS),
@@ -14,4 +16,8 @@ test_x, test_y = prepare_dataset(
     os.path.join(settings.CNN_DATA_DIR, settings.CNN_TEST_LABELS),
     settings.CNN_TEST_FILES_PATH)
 
-embeddings = get_word_embeddings(os.path.join(settings.CNN_MODEL_PATH, settings.CNN_MODEL_NAME), train_x, validation_x)
+embedding_matrix, word_index, train_seq_x, validation_seq_x = get_word_embeddings(os.path.join(settings.CNN_MODEL_PATH, settings.CNN_MODEL_NAME), train_x, validation_x)
+
+classifier = cnn(word_index, embedding_matrix)
+accuracy = train_model(classifier, train_x, train_y, validation_x, validation_y)
+print (f"CNN, Word Embeddings {accuracy}")
---
+++ scripts/networks.py
@@ -0,0 +1,15 @@
+from keras import layers, models, optimizers
+
+
+def cnn(word_index, embedding_matrix):
+    input_layer = layers.Input((70,))
+    embedding_layer = layers.Embedding(len(word_index) + 1, 100, weights=[embedding_matrix], trainable=False)(input_layer)
+    embedding_layer = layers.SpatialDropout1D(0.3)(embedding_layer)
+    convolution_layer = layers.Convolution1D(100, 3, activation="relu")(embedding_layer)
+    pooling_layer = layers.GlobalMaxPool1D()(convolution_layer)
+    output_layer1 = layers.Dense(50, activation="relu")(pooling_layer)
+    output_layer1 = layers.Dropout(0.25)(output_layer1)
+    output_layer2 = layers.Dense(1, activation="sigmoid")(output_layer1)
+    model = models.Model(inputs=input_layer, outputs=output_layer2)
+    model.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy')
+    return model
