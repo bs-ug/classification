5cae49907198b3c8acac35e8f421ce9fd7f9201a
Bartosz Sochaj <bartosz.sochaj@oke.pl>
2019-04-28 17:16:50 +0200

    rnn

--- scripts/cnn_train.py
+++ scripts/cnn_train.py
@@ -5,7 +5,7 @@ from keras.utils import to_categorical
 from scripts import settings
 from scripts.cnn_datasets import prepare_dataset
 from scripts.cnn_embedings import get_word_embeddings
-from scripts.networks import cnn, simple
+from scripts.networks import cnn, simple, rnn
 from scripts.utils import train_model
 
 train_x, train_y = prepare_dataset(
@@ -22,16 +22,20 @@ validation_y = to_categorical(validation_y)
 test_y = to_categorical(test_y)
 
 embedding_matrix, word_index, train_seq_x, validation_seq_x = get_word_embeddings(
-    os.path.join(settings.CNN_MODEL_PATH, settings.CNN_MODEL_NAME), train_x, validation_x)
+    os.path.join(settings.CNN_MODEL_PATH, "glove.42B.300d.txt"), train_x, validation_x)
 
 # TODO: select network to train via script params
-classifier = cnn(word_index, embedding_matrix)
-print(classifier.summary())
-train_model(classifier, train_seq_x, train_y, validation_seq_x, validation_y, batch_size=512, epochs=1,
-            model_name="cnn_model.hdf5")
+# classifier = cnn(word_index, embedding_matrix, len(settings.CNN_TOPICS))
+# print(classifier.summary())
+# train_model(classifier, train_seq_x, train_y, validation_seq_x, validation_y, batch_size=128, epochs=50,
+#             model_name="cnn_model.hdf5")
+#
+# classifier = simple(word_index, embedding_matrix, len(settings.CNN_TOPICS))
+# print(classifier.summary())
+# train_model(classifier, train_seq_x, train_y, validation_seq_x, validation_y, batch_size=128, epochs=50,
+#             model_name="simple_model.hdf5")
 
-classifier = simple(word_index, embedding_matrix)
+classifier = rnn(word_index, embedding_matrix, len(settings.CNN_TOPICS))
 print(classifier.summary())
-train_model(classifier, train_seq_x, train_y, validation_seq_x, validation_y, batch_size=512, epochs=10,
-            model_name="simple_model.hdf5")
-
+train_model(classifier, train_seq_x, train_y, validation_seq_x, validation_y, batch_size=128, epochs=50,
+            model_name="rnn_model.hdf5")
--- scripts/networks.py
+++ scripts/networks.py
@@ -3,43 +3,42 @@ from keras import layers, models, optimizers
 from scripts import settings
 
 
-def cnn(word_index, embedding_matrix):
+def cnn(word_index, embedding_matrix, number_of_classes):
     input_layer = layers.Input((1881,))
     embedding_layer = layers.Embedding(
         len(word_index) + 1,
         settings.EMBEDDINGS_VECTOR_LENGTH,
         weights=[embedding_matrix],
         trainable=False)(input_layer)
-    embedding_layer = layers.SpatialDropout1D(0.3)(embedding_layer)
-    convolution_layer = layers.Convolution1D(100, 3, activation="relu")(embedding_layer)
+    embedding_layer = layers.SpatialDropout1D(0.1)(embedding_layer)
+    convolution_layer = layers.Convolution1D(200, 3, activation="relu")(embedding_layer)
     pooling_layer = layers.GlobalMaxPool1D()(convolution_layer)
     output_layer1 = layers.Dense(50, activation="relu")(pooling_layer)
-    output_layer1 = layers.Dropout(0.3)(output_layer1)
-    output_layer2 = layers.Dense(15, activation="sigmoid")(output_layer1)
+    output_layer1 = layers.Dropout(0.1)(output_layer1)
+    output_layer2 = layers.Dense(number_of_classes, activation="sigmoid")(output_layer1)
     model = models.Model(inputs=input_layer, outputs=output_layer2)
     model.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 
-def cnn2(word_index, embedding_matrix):
-    model = models.Sequential()
-    model.add(layers.InputLayer((1881,)))
-    model.add(layers.Embedding(
+def rnn(word_index, embedding_matrix, number_of_classes):
+    input_layer = layers.Input((1881,))
+    embedding_layer = layers.Embedding(
         len(word_index) + 1,
         settings.EMBEDDINGS_VECTOR_LENGTH,
         weights=[embedding_matrix],
-        trainable=False))
-    model.add(layers.SpatialDropout1D(0.3))
-    model.add(layers.Convolution1D(100, 3, activation="relu"))
-    model.add(layers.GlobalMaxPool1D())
-    model.add(layers.Dense(50, activation="relu"))
-    model.add(layers.Dropout(0.1))
-    model.add(layers.Dense(15, activation="sigmoid"))
+        trainable=False)(input_layer)
+    embedding_layer = layers.SpatialDropout1D(0.1)(embedding_layer)
+    lstm_layer = layers.LSTM(100)(embedding_layer)
+    output_layer1 = layers.Dense(50, activation="relu")(lstm_layer)
+    output_layer1 = layers.Dropout(0.1)(output_layer1)
+    output_layer2 = layers.Dense(number_of_classes, activation="sigmoid")(output_layer1)
+    model = models.Model(inputs=input_layer, outputs=output_layer2)
     model.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 
-def simple(word_index, embedding_matrix):
+def simple(word_index, embedding_matrix, number_of_classes):
     model = models.Sequential()
     model.add(layers.Embedding(
         len(word_index) + 1,
@@ -48,6 +47,6 @@ def simple(word_index, embedding_matrix):
         input_length=1881,
         trainable=False))
     model.add(layers.Flatten())
-    model.add(layers.Dense(15, activation="sigmoid"))
+    model.add(layers.Dense(number_of_classes, activation="sigmoid"))
     model.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
     return model
